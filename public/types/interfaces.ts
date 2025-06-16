1334a7edeb702f5906b1a74d2fdd63b23840e76f
Aslan <abubakarovdev@gmail.com>
2025-06-16 16:40:54 +0300

    Настройка плейлистов

--- public/types/interfaces.ts
+++ public/types/interfaces.ts
@@ -10,30 +10,6 @@ export interface PlaylistItem {
 }
 
 
-{
-    "id": "938ca8a5-a531-43a5-949b-8cb7616a617a",
-    "name": "My Playlist TEST UPDATE",
-    "organizationId": "cb963d92-c9df-4107-ada1-4473a52d1a34",
-    "createdBy": "979ffbcb-3a74-40b6-8c87-837ccd244380",
-    "childFiles": [
-    {
-        "id": "dcd6a421-ba03-4497-87b5-86c45a96cc88",
-        "orderIndex": 0,
-        "name": "sadasdas",
-        "previewUrl": "2025/6/preview/preview_sadasdas.jpg",
-        "type": "video/quicktime",
-        "duration": 5.935967
-    },
-    {
-        "id": "604aa174-0072-4bc7-9b99-c966d9c74d5a",
-        "orderIndex": 1,
-        "name": "Hydrolic Press 5 M=.mp4",
-        "previewUrl": "",
-        "type": "video/mp4",
-        "duration": 303.2029
-    }
-]
-}
 
 export interface FileItem {
     id: string;
