a2ffbf9b63f92b710fc0fcdf0fd474bd08b7d2bc
xvishnevskix <xvishnevskix@mail.ru>
2025-11-14 13:17:43 +0400

    правки путей

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'admin.screon.ru',
+                hostname: 'screon.videotrade.ru',
                 port: '',
                 pathname: '/api/files/**',
             },
